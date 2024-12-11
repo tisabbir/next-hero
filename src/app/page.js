5bd411f227301732718b141bce0dcb31877fe56a
TI Sabbir <tisabbir0@gmail.com>
2024-12-11 23:52:48 +0600

    heading problem solved

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
-import heading from "@/components/heading";
+import Heading from "@/components/Heading";
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
+      <Heading />
     </div>
   );
 }
