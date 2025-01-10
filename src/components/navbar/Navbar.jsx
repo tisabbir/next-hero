b96dc76e99aba2a943929fc0205d89a56ceae629
TI Sabbir <tisabbir0@gmail.com>
2025-01-10 11:24:13 +0600

    handle login

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 "use client"
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
 
 const Navbar = () => {
 
     const pathName = usePathname();
+    const router = useRouter();
     const links = [
         {
             title : "About",
@@ -20,14 +21,19 @@ const Navbar = () => {
             path : "/contact"
         }
     ]
+
+    const handleLogin = () =>{
+        router.push('/login');
+    };
     return (
-        <div className='bg-red-500 text-white flex justify-between px-6'>
+        <div className='bg-red-500 text-white flex justify-between px-6 py-6'>
             <Link className='text-xl' href="/">Next Hero</Link>
            <ul className='flex justify-between gap-4'>
             {
                 links.map((link) => <Link className={`${pathName === link.path && "text-black"}`} key={link.path} href={link.path}>{link.title}</Link>)
             }
            </ul>
+           <button onClick={handleLogin} className='bg-white text-black px-4 rounded-lg'>Login</button>
         </div>
     );
 };
