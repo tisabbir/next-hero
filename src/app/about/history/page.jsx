9321b835c557bfc2cab28e42a14a8a16602c0240
TI Sabbir <tisabbir0@gmail.com>
2025-01-23 11:07:02 +0600

    add : nested route, history and mission addded

---
+++ src/app/about/history/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is history page
+        </div>
+    );
+};
+
+export default page;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,13 @@
+import Link from 'next/link';
 import React from 'react';
 
 const AboutPage = () => {
     return (
         <div>
-            About
+            About Page
+
+            <Link href={'/about/history'}>History</Link>
+            <Link href={'/about/mission'}>Mission</Link>
         </div>
     );
 };
