b8b07bff275f9d7f57a6c657d09595982e49dd2d
TI Sabbir <tisabbir0@gmail.com>
2025-01-25 21:47:50 +0600

    Add : group route

---
+++ src/app/(dashboards)/column/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            this is column bro
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(dashboards)/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            this is dashboard bro?
+        </div>
+    );
+};
+
+export default page;
