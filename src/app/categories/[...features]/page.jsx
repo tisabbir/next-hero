1601103b69de60656c9f8a7ee243a7982aa03ec3
TI Sabbir <tisabbir0@gmail.com>
2025-01-29 10:18:24 +0600

    add : game page

--- src/app/categories/[...features]/page.jsx
+++ src/app/categories/[...features]/page.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const CategoriesDetailsPage = () => {
+const CategoriesDetailsPage = ({params}) => {
+    if(params.features.length == 3){
+        return <div>{params.features[2]}</div>
+    }
     return (
         <div>
             This is categories details page
---
+++ src/app/games/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            This is game page.
+        </div>
+    );
+};
+
+export default page;
